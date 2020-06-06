544488f5031914475768f84fa5eeefd60aa8a9d7
lill74 <a0103932@naver.com>
2020-06-06 16:18:14 +0900

    Upadete

--- app.py
+++ app.py
@@ -4,11 +4,15 @@ from flask import Flask, request, redirect
 import urllib.parse
 import requests
 import json
+import uuid
+import redis
 import secrets
+
+r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 
 app = Flask(__name__)
 
-scopes = 'user-read-currently-playing'
+scopes = 'user-read-playback-state'
 redirect_url = 'http://localhost:5000/callback'
 spotify_auth = 'https://accounts.spotify.com/authorize'
 spotify_token = 'https://accounts.spotify.com/api/token'
@@ -24,7 +28,7 @@ def login():
 @app.route('/callback')
 def callback():
     data = {
-        'grant_type': 'authorization_code',
+        'grant_type' : 'authorization_code',
         'code': request.args.get('code'),
         'redirect_uri': redirect_url,
         'client_id' : my_client_id,
@@ -32,8 +36,22 @@ def callback():
     }
 
     req = requests.post(spotify_token, data=data)
+    unique_id = str(uuid.uuid4())
 
-    return json.loads(req.text)['access_token']
+    r.set(unique_id, json.loads(req.text)['access_token'])
+
+    return unique_id + ' ' + json.loads(req.text)['access_token']
+
+@app.route('/player-status')
+def playerStatus():
+
+    headers = {
+        'Authorization': 'Bearer ' + r.get(request.args.get('token'))
+    }
+
+    req = requests.get('https://api.spotify.com/v1/me/player', headers=headers)
+
+    return req.text
 
 if __name__ == '__main__':
     app.debug = True
